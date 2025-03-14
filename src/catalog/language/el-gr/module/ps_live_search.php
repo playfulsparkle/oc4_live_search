c9c9efa8327417052aa32783827463494c530bb5
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2025-03-14 11:28:30 +0100

    Added Ελληνικά (el-GR), العربية (ar), Polski (pl-pl) languages

---
+++ src/catalog/language/el-gr/module/ps_live_search.php
@@ -0,0 +1,11 @@
+<?php
+// Heading
+$_['heading_products']         = 'Προϊόντα';
+$_['heading_categories']       = 'Κατηγορίες';
+$_['heading_manufacturers']    = 'Κατασκευαστές';
+$_['heading_informations']     = 'Πληροφορίες';
+
+// Text
+$_['text_showing_results']     = 'Εμφάνιση αποτελεσμάτων για <strong id="ps-live-search-query"></strong>';
+$_['text_all_product_results'] = 'Εμφάνιση όλων των αποτελεσμάτων προϊόντων';
+$_['text_no_results']          = 'Κανένα αποτέλεσμα';
---
+++ src/catalog/language/pl-pl/module/ps_live_search.php
@@ -0,0 +1,11 @@
+<?php
+// Heading
+$_['heading_products']         = 'Produkty';
+$_['heading_categories']       = 'Kategorie';
+$_['heading_manufacturers']    = 'Producenci';
+$_['heading_informations']     = 'Informacje';
+
+// Text
+$_['text_showing_results']     = 'Wyświetlanie wyników dla <strong id="ps-live-search-query"></strong>';
+$_['text_all_product_results'] = 'Pokaż wszystkie wyniki produktów';
+$_['text_no_results']          = 'Brak wyników';
