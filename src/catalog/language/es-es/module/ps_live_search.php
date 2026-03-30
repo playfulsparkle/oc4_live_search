5ee82fefc384cac2542700026b91068db6f8861f
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2026-03-30 12:39:40 +0200

    Update dev dependencies, add missing translations

---
+++ src/catalog/language/es-es/module/ps_live_search.php
@@ -0,0 +1,11 @@
+<?php
+// Heading
+$_['heading_products']      = 'Productos';
+$_['heading_categories']    = 'Categorías';
+$_['heading_manufacturers'] = 'Fabricantes';
+$_['heading_informations']  = 'Información';
+
+// Text
+$_['text_showing_results']      = 'Mostrando resultados para <output></output>';
+$_['text_all_product_results']  = 'Mostrar todos los resultados de productos';
+$_['text_no_results']           = 'Sin resultados';
---
+++ src/catalog/language/fr-fr/module/ps_live_search.php
@@ -0,0 +1,11 @@
+<?php
+// Heading
+$_['heading_products']      = 'Produits';
+$_['heading_categories']    = 'Catégories';
+$_['heading_manufacturers'] = 'Fabricants';
+$_['heading_informations']  = 'Informations';
+
+// Text
+$_['text_showing_results']      = 'Affichage des résultats pour <output></output>';
+$_['text_all_product_results']  = 'Afficher tous les résultats de produits';
+$_['text_no_results']           = 'Aucun résultat';
